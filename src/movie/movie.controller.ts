1afe63f7d824308b4d51c5df8974a7454235a1a1
itsidorkin28 <itsidorkin28@gmail.com>
2022-06-09 02:23:27 +0300

    movie crud

---
+++ src/movie/movie.controller.ts
@@ -0,0 +1,94 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { IdValidationPipe } from '../pipes/id.validation.pipe'
+import { MovieService } from './movie.service'
+import { UpdateMovieDto } from './dto/update-movie.dto'
+import { Types } from 'mongoose'
+import { GenreIdsDto } from './dto/genreIds.dto'
+
+@Controller('movies')
+export class MovieController {
+	constructor(private readonly movieService: MovieService) {
+	}
+
+	@Get('by-slug/:slug')
+	async bySlug(@Param('slug') slug: string) {
+		return this.movieService.bySlug(slug)
+	}
+
+	@Get('by-actor/:actorId')
+	async byActor(@Param('actorId', IdValidationPipe) actorId: Types.ObjectId) {
+		return this.movieService.byActor(actorId)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post('by-genres')
+	@HttpCode(200)
+	async byGenres(
+		@Body()
+			{ genreIds }: GenreIdsDto
+	) {
+		return this.movieService.byGenres(genreIds);
+	}
+
+
+	@Get()
+	async getAll(@Query('searchTerm') searchTerm?: string) {
+		return this.movieService.getAll(searchTerm)
+	}
+
+	@Get('most-popular')
+	async getMostPopular() {
+		return this.movieService.getMostPopular()
+	}
+
+	@Put('update-count-opened')
+	@HttpCode(200)
+	async updateCountOpened(@Body('slug') slug: string) {
+		return await this.movieService.updateCountOpened(slug)
+	}
+
+	@Get(':id')
+	@Auth('admin')
+	async get(@Param('id', IdValidationPipe) id: string) {
+		return this.movieService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	async create() {
+		return this.movieService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: UpdateMovieDto,
+	) {
+		return await this.movieService.update(id, dto)
+	}
+
+	@Delete(':id')
+	@Auth('admin')
+	@HttpCode(200)
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		return this.movieService.delete(id)
+	}
+}
---
+++ src/movie/movie.service.ts
@@ -0,0 +1,126 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectModel } from 'nestjs-typegoose'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { MovieModel } from './movie.model'
+import { UpdateMovieDto } from './dto/update-movie.dto'
+import { Types } from 'mongoose'
+
+@Injectable()
+export class MovieService {
+	constructor(@InjectModel(MovieModel) private readonly MovieModel: ModelType<MovieModel>) {
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {}
+		if (searchTerm) {
+			options = {
+				$or: [
+					{
+						title: new RegExp(searchTerm, 'i'),
+					},
+				],
+			}
+		}
+
+		return this.MovieModel
+			.find(options)
+			.select('-updatedAt -__v')
+			.sort({
+				createdAt: 'desc',
+			})
+			.populate('actors genres')
+			.exec()
+	}
+
+	async byActor(actorId: Types.ObjectId) {
+		const docs = await this.MovieModel.findOne({ actors: actorId }).exec()
+		if (!docs) throw new NotFoundException('Movies not found!')
+		return docs
+	}
+
+	async byGenres(genreIds: Types.ObjectId[]) {
+		const doc = await this.MovieModel.find({
+			genres: { $in: genreIds },
+		}).exec()
+		if (!doc) throw new NotFoundException('Movies not found!')
+		return doc
+	}
+
+	async bySlug(slug: string) {
+		const doc = await this.MovieModel.findOne({ slug }).populate('actors genres').exec()
+		if (!doc) throw new NotFoundException('Movies not found!')
+		return doc
+	}
+
+	async updateCountOpened(slug: string) {
+		const updateDoc = await this.MovieModel
+			.findOneAndUpdate({ slug },
+				{
+					$inc: { countOpened: 1 },
+				},
+				{
+					new: true,
+				})
+			.exec()
+
+		if (!updateDoc) {
+			return new NotFoundException('Movie not found')
+		}
+
+		return updateDoc
+	}
+
+	async getMostPopular() {
+		return await this.MovieModel
+			.find({ countOpened: { $gt: 0 } })
+			.sort({ countOpened: -1 })
+			.populate('genres')
+			.exec()
+	}
+
+	/* Admin place */
+
+	async byId(_id: string) {
+		const doc = await this.MovieModel.findById(_id)
+		if (!doc) throw new NotFoundException('Movie not found')
+		return doc
+	}
+
+	async update(_id: string, dto: UpdateMovieDto) {
+
+		//todo: Telegram notifications
+		const updateDoc = await this.MovieModel.findByIdAndUpdate(_id, dto, {
+			new: true,
+		}).exec()
+
+		if (!updateDoc) {
+			return new NotFoundException('Movie not found')
+		}
+
+		return updateDoc
+	}
+
+	async create() {
+		const defaultValue: UpdateMovieDto = {
+			bigPoster: '',
+			actors: [],
+			genres: [],
+			poster: '',
+			title: '',
+			videoUrl: '',
+			slug: '',
+		}
+		const movie = await this.MovieModel.create(defaultValue)
+		return movie._id
+	}
+
+	async delete(id: string) {
+		const deleteDoc = await this.MovieModel.findByIdAndDelete(id)
+
+		if (!deleteDoc) {
+			return new NotFoundException('Movie not found')
+		}
+
+		return deleteDoc
+	}
+}
