963691b14bcbb1187b96a273cde730d28df02885
itsidorkin28 <itsidorkin28@gmail.com>
2022-06-09 00:33:13 +0300

    actor crud

---
+++ src/actor/actor.controller.ts
@@ -0,0 +1,66 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { IdValidationPipe } from '../pipes/id.validation.pipe'
+import { ActorService } from './actor.service'
+import { ActorDto } from './actor.dto'
+
+@Controller('actors')
+export class ActorController {
+	constructor(private readonly actorService: ActorService) {
+	}
+
+	@Get('by-slug/:slug')
+	async bySlug(@Param('slug') slug: string) {
+		return this.actorService.bySlug(slug)
+	}
+
+
+	@Get()
+	async getAll(@Query('searchTerm') searchTerm?: string) {
+		return this.actorService.getAll(searchTerm)
+	}
+
+	@Get(':id')
+	@Auth('admin')
+	async get(@Param('id', IdValidationPipe) id: string) {
+		return this.actorService.byId(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: ActorDto,
+	) {
+		return await this.actorService.update(id, dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	async create() {
+		return this.actorService.create()
+	}
+
+	@Delete(':id')
+	@Auth('admin')
+	@HttpCode(200)
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		return this.actorService.delete(id)
+	}
+}
